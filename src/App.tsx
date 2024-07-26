142bfd27e7248a4467addc65206c2e8c2a62ab14
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-07-26 10:12:38 +0200

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,61 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Booking = lazy(() => import("./pages/Booking"));
+const Gallery = lazy(() => import("./pages/Gallery"));
+const Information = lazy(() => import("./pages/Information"));
+const LegalNotice = lazy(() => import("./pages/LegalNotice"));
+
+const routes = [
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/booking",
+    element: <Booking />,
+  },
+  {
+    path: "/gallery",
+    element: <Gallery />,
+  },
+  {
+    path: "/information",
+    element: <Information />,
+  },
+  {
+    path: "/legal-notice",
+    element: <LegalNotice />,
+  },
+];
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <Homepage></Homepage>
+            </Suspense>
+          }
+        />
+        {routes.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                {route.element}
+              </Suspense>
+            }
+          />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
