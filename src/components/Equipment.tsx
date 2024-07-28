b896b8ee192afebbe3f3f2fa6a36d08a0a8a3c55
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-07-28 09:27:50 +0200

    removed equipment accordion

--- src/components/Equipment.tsx
+++ src/components/Equipment.tsx
@@ -1,6 +1,3 @@
-import * as Accordion from "@radix-ui/react-accordion";
-import { ChevronDownIcon } from "@radix-ui/react-icons";
-
 const data = [
   {
     title: "General equipment",
@@ -45,43 +42,7 @@ const data = [
 ];
 
 function Equipment() {
-  return (
-    <div>
-      <Accordion.Root
-        className="w-2/3 p-8 mx-auto rounded-md bg-base-200"
-        type="single"
-        defaultValue="item"
-        collapsible
-      >
-        {data.map((element, index) => (
-          <Accordion.Item
-            value={element.title}
-            key={index}
-            className="overflow-hidden"
-          >
-            <Accordion.Header className="flex">
-              <Accordion.Trigger className="flex items-center justify-between flex-1 text-2xl cursor-default group font-accent text-tertiary">
-                {element.title}
-                <ChevronDownIcon
-                  className="ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:rotate-180"
-                  width={25}
-                  height={25}
-                />
-              </Accordion.Trigger>
-            </Accordion.Header>
-
-            <Accordion.Content className="data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp overflow-hidden">
-              {element.infos.map((info, index) => (
-                <p className="" key={index}>
-                  {info}
-                </p>
-              ))}
-            </Accordion.Content>
-          </Accordion.Item>
-        ))}
-      </Accordion.Root>
-    </div>
-  );
+  return <div>Equipment</div>;
 }
 
 export default Equipment;
