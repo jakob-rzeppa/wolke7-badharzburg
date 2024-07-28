1f038007f49c9ade77ae7439982cd6a83107a658
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-07-28 15:50:56 +0200

    refactored the Map Section

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -11,7 +11,13 @@ function Homepage() {
         content="We warmly welcome you to our “Wolke7” in Bad Harzburg. We wish you a restful and relaxing holiday in the Harz Mountains. "
         link={{ href: "/booking", name: "Check avaliability" }}
       />
-      <Map />
+      <Map
+        title="Location"
+        content=" Our charming holiday apartment “Wolke7” welcomes you in Bad Harzburg, 1.7 kilometres from the center of the spa town of Bad Harzburg."
+        address="Am Breitenberg 41/3, 38667 Bad Harzburg"
+        latitude={51.87879012975221}
+        longitude={10.538890025373371}
+      />
     </>
   );
 }
--- src/sections/Map.tsx
+++ src/sections/Map.tsx
@@ -1,31 +1,42 @@
 import classNames from "classnames";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
-interface Props extends React.AllHTMLAttributes<HTMLDivElement> {}
+interface Props extends React.AllHTMLAttributes<HTMLDivElement> {
+  title: string;
+  content: string;
+  address: string;
+  latitude: number;
+  longitude: number;
+}
 
-function Map({ className, ...props }: Props) {
+function Map({
+  title,
+  content,
+  address,
+  latitude,
+  longitude,
+  className,
+  ...props
+}: Props) {
   return (
     <section
       className={classNames(className, "w-screen bg-base-200 text-center p-8")}
       {...props}
     >
-      <h1 className="text-3xl font-accent text-secondary">Location</h1>
-      <p className="mt-4">
-        Our charming holiday apartment “Wolke7” welcomes you in Bad Harzburg,
-        1.7 kilometres from the center of the spa town of Bad Harzburg.
-      </p>
+      <h1 className="text-3xl font-accent text-secondary">{title}</h1>
+      <p className="mt-4">{content}</p>
       <div className="relative py-4">
         <div className="absolute inset-0 flex items-center">
           <div className="w-full border-b border-primary"></div>
         </div>
         <div className="relative flex justify-center">
           <span className="px-4 text-sm bg-base-200 text-primary">
-            Am Breitenberg 41/3, 38667 Bad Harzburg
+            {address}
           </span>
         </div>
       </div>
       <MapContainer
-        center={[51.87879012975221, 10.538890025373371]}
+        center={[latitude, longitude]}
         zoom={15}
         scrollWheelZoom={false}
         className="w-full aspect-[16/9] rounded-lg z-10"
@@ -34,8 +45,8 @@ function Map({ className, ...props }: Props) {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.87879012975221, 10.538890025373371]}>
-          <Popup>Am Breitenberg 41/3, 38667 Bad Harzburg, Deutschland</Popup>
+        <Marker position={[latitude, longitude]}>
+          <Popup>{address}</Popup>
         </Marker>
       </MapContainer>
     </section>
