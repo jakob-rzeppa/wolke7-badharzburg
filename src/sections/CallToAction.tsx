bc55c261e55db30f03e53f4ad226a191ccd34cf1
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-07-30 14:29:13 +0200

    redid call to action

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import ActionLink from "../components/ActionLink";
 import Section, { SectionProps } from "../components/Section";
 
@@ -11,13 +12,13 @@ interface Props extends SectionProps {
 
 function CallToAction({ text, link, className, background }: Props) {
   return (
-    <Section className={className} background={background}>
-      <section className="flex flex-col gap-4 p-4 m-16 text-center rounded-lg bg-secondary text-secondary-content md:p-16">
+    <Section className={classNames(className, "px-16")} background={background}>
+      <div className="flex flex-col gap-4 px-4 py-8 text-center rounded-lg bg-secondary text-secondary-content md:p-16">
         <h1 className="text-4xl">{text}</h1>
         <ActionLink to={link.href} className="mx-auto bg-tertiary w-max">
           {link.name}
         </ActionLink>
-      </section>
+      </div>
     </Section>
   );
 }
