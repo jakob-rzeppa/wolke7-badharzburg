0be9e897d2b61af9257541b3d97216c47bd183b1
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-08-04 10:28:59 +0200

    first grid gallery

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -1,36 +1,10 @@
-import { Carousel } from "flowbite-react";
+import Gallery from "../sections/Gallery";
 
 function GalleryPage() {
   return (
-    <div className="w-screen h-screen">
-      <Carousel slideInterval={3000} className="w-2/3 m-auto h-2/3">
-        <img
-          src="/pictures/Schlafzimmer/Doppelbett.jpg"
-          alt="..."
-          className="aspect-square"
-        />
-        <img
-          src="/pictures/Schlafzimmer/Doppelbett.jpg"
-          alt="..."
-          className="aspect-square"
-        />
-        <img
-          src="/pictures/Schlafzimmer/Doppelbett.jpg"
-          alt="..."
-          className="aspect-square"
-        />
-        <img
-          src="/pictures/Schlafzimmer/Doppelbett.jpg"
-          alt="..."
-          className="aspect-square"
-        />
-        <img
-          src="/pictures/Schlafzimmer/Doppelbett.jpg"
-          alt="..."
-          className="aspect-square"
-        />
-      </Carousel>
-    </div>
+    <>
+      <Gallery />
+    </>
   );
 }
 
